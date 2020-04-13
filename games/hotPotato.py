041ab7a01f7c8fc867d3ea85bcc3e4e963de2cf4
Alex Kigen <61216190+alexkigen@users.noreply.github.com>
2020-04-12 22:24:02 -0400

    Create hotPotato.py

---
+++ games/hotPotato.py
@@ -0,0 +1,36 @@
+#implement hot potato game
+
+numberOfPlayers = int(input("Enter the number of Players: "))
+
+playerNames = list(range(numberOfPlayers))
+
+for i in range(numberOfPlayers):
+    print ("Player ", i+1 ,", enter your name")
+    playerNames[i] = input("Player name: ")
+
+print ("Player names and orders are as follows: ", playerNames)
+
+#hot potato engine:
+#pass potato each 2 seconds; randomly stop anywhere before 30 seconds
+
+#method A - base Python
+time = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+
+import random
+import math
+countRemain = len(playerNames)
+playersLeft = playerNames
+
+print ("Starting players: ", playersLeft)
+
+while countRemain > 1:
+    chosen = random.choice(time)
+    #print (chosen)
+    group = math.ceil(chosen/len(playersLeft))
+    #print (group)
+    losingPlayerIndex = chosen - ((group-1)*len(playersLeft)) - 1
+    #print (losingPlayerIndex)
+    losingPlayer = playersLeft[losingPlayerIndex]
+    playersLeft.remove(losingPlayer)
+    print ("Players left: ", playersLeft)
+    countRemain -= 1
